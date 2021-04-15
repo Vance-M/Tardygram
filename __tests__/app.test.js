2fc6c5ad6efc0c46786c54c5a3be1f576fc8c9d1
Vance-M <myveryvills@yahoo.com>
2021-04-15 10:41:15 -0700

    tested and working postgram route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,36 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-
+const User = require('../lib/models/User');
 describe('. routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  beforeEach(async () => {
+    await User.insert({ username: 'Bob', photoURL: 'fakephoto' })
+  })
+
+
+  it('posts a postgram to table', () => {
+    return request(app)
+      .post('/api/v1/post')
+      .send({
+        username: 'Bob',
+        photoURL: 'fakephoto',
+        caption: 'Its a fake photo',
+        tags: `{ fake, news }`,
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          postgram_id: 1,
+          username: 'Bob',
+          photoURL: 'fakephoto',
+          caption: 'Its a fake photo',
+          tags: [ 'fake', 'news' ]
+        })
+      })
+  })
+
+
 });
